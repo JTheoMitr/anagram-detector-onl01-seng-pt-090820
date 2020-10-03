50ca882d2989918f8c973dacd6f9ffe4ba565184
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-03 19:10:51 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   end
   
   def match(array)
-    array.select { |string| (string.split("").sort) == (@word.split("").sort) }
+    array.select { |string| (string.split("").sort) == (word.split("").sort) }
   end
   
 end
